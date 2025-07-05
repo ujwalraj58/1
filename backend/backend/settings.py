00a014502024d5c0c7b4f53a35b036bdc7479030
ujwalraj58 <ujwalrajdussa@gmail.com>
2025-07-05 15:00:21 +0530

    2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
